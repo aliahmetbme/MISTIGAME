49acccd2db31282da8f5b2574c48d478097f841d
aliahmetbme <082aliahmet4@gmail.com>
2023-04-20 22:29:26 +0300

    card class editted

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,6 @@
 public class Card {
+    private String suit;
+    private String cardFace;
+    private int points;
+
 }
---
+++ src/Deck.java
@@ -0,0 +1,5 @@
+import java.util.ArrayList;
+
+public class Deck {
+    ArrayList<Card> cards;
+}
