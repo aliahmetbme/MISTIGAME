870351f3cc409136a27320d63752153ca7084098
aliahmetbme <082aliahmet4@gmail.com>
2023-05-02 02:48:32 +0300

    ss

--- src/BothPlayer.java
+++ src/BothPlayer.java
@@ -2,15 +2,10 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public abstract class BothPlayer extends Player{
-<<<<<<< HEAD
+
     public BothPlayer(String name, ArrayList<Card> hand, int score, String level) {
         super(name, hand, score, level);
-=======
 
-    public BothPlayer(String name, ArrayList<Card> hand, int score) {
-        super(name, hand, score);
->>>>>>> origin/ayça
     }
-
     public abstract Card playCard() ;
 }
